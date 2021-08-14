799d81dc77355c7329e756e0adbfde1f6361928b
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-14 13:14:02 -0400

    Add files via upload

---
+++ Warmup-1/makes10.py
@@ -0,0 +1,34 @@
+#CodingBat - Python
+
+#makes10
+
+
+#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes10(a, b):
+    return True if a + b == 10 or (a == 10 or b == 10) else False
+
+print(makes10(9, 10))
+print(makes10(9, 9))
+print(makes10(1, 9))
+# =============================================================================
+
+# def makes10(a, b):
+#     if a + b == 10:
+#         return True
+#     elif a == 10 or b == 10:
+#         return True
+#     else:
+#         return False
+
+
+# def makes10(a, b):
+#     if a + b == 10 or a == 10 or b == 10:
+#         return True
+#     else:
+#         return False
+    
