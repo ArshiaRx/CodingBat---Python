4379bb809433d03e3ea09ac06a167b2cb18c70ef
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-13 22:24:43 -0400

    Update pos_neg.py

--- Warmup-1/pos_neg.py
+++ Warmup-1/pos_neg.py
@@ -15,8 +15,7 @@ def pos_neg(a, b, negative):
         return (a < 0 and b < 0)
     else:
         return ((a < 0 and b > 0) or (a > 0 and b < 0))
-    
-        
+            
 print(pos_neg(1, -1, False))
 print(pos_neg(-1, 1, False))
 print(pos_neg(-4, -5, True))
