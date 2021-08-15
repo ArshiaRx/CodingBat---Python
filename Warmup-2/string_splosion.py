b9decbad2c414d1803fb81342c1ebade9342e6d2
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-15 13:20:27 -0400

    Add files via upload

---
+++ Warmup-2/string_splosion.py
@@ -0,0 +1,20 @@
+#CodingBat - Python
+
+#string_splosion
+
+#Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+# string_splosion('Code') → 'CCoCodCode'
+# string_splosion('abc') → 'aababc'
+# string_splosion('ab') → 'aab'
+
+def string_splosion(str):
+  result = ""
+  for i in range(1, len(str) + 1):
+    result += str[:i]
+   
+  return result
+
+print(string_splosion('Code'))
+print(string_splosion('abc'))
+print(string_splosion('ab'))
