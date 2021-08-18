4e669b6212de84b4cbba6a98a5a34fbdcc5864ba
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-18 17:40:12 -0400

    Update front_times.py

--- Warmup-2/front_times.py
+++ Warmup-2/front_times.py
@@ -1,9 +1,10 @@
 #CodingBat - Python
 
+#front_times
+
 #Given a string and a non-negative int n, we'll say that the front of the 
 #string is the first 3 chars, or whatever is there if the string is less than
 #length 3. Return n copies of the front;
-
 
 # front_times('Chocolate', 2) → 'ChoCho'
 # front_times('Chocolate', 3) → 'ChoChoCho'
@@ -15,10 +16,10 @@ def front_times(str, n):
     else:
         return str * 3
     
-    
-print(front_times('Chocolate', 2))
-print(front_times('Chocolate', 3))
-print(front_times('Abc', 3))
+#To check    
+#print(front_times('Chocolate', 2))
+#print(front_times('Chocolate', 3))
+#print(front_times('Abc', 3))
 # =============================================================================
 
 # def front_times(str, n):
