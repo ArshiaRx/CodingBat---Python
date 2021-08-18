a4de45dac81655f5ff8cb10f559ddc98d45707a1
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-18 16:59:11 -0400

    Add files via upload

---
+++ Warmup-2/string_bits.py
@@ -0,0 +1,22 @@
+#CodingBat - Python
+
+#Given a string, return a new string made of every other char starting with the
+#first, so "Hello" yields "Hlo".
+
+# string_bits('Hello') → 'Hlo'
+# string_bits('Hi') → 'H'
+# string_bits('Heeololeo') → 'Hello'
+
+
+def string_bits(str):
+  result = ''
+  string = len(str)
+  
+  for i in range(string):
+    if i % 2 == 0:
+      result = result + str[i]
+  return result
+
+print(string_bits('Hello'))
+print(string_bits('Hi'))
+print(string_bits('Heeololeo'))
