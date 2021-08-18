8337094f1cdebcc31aa549785ba14ae322753f78
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-18 17:37:42 -0400

    Update array_front9.py

--- Warmup-2/array_front9.py
+++ Warmup-2/array_front9.py
@@ -1,5 +1,7 @@
 #CodingBat - Python 
 
+#array_front9
+
 #Given an array of ints, return True if one of the first 4 elements in the 
 #array is a 9. The array length may be less than 4.
 
@@ -11,9 +13,10 @@ def array_front9(nums):
     #Boolean method (quick method)
     return 9 in nums[:4]
 
-print(array_front9([1, 2, 9, 3, 4]))
-print(array_front9([1, 2, 3, 4, 9]))
-print(array_front9([1, 2, 3, 4, 5]))
+#To check
+#print(array_front9([1, 2, 9, 3, 4]))
+#print(array_front9([1, 2, 3, 4, 9]))
+#print(array_front9([1, 2, 3, 4, 5]))
 # =============================================================================
 
 # def array_front9(nums):
