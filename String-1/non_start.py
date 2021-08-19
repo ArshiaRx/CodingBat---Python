2bc0d598d2d69581fd5a6df26e21bf5ccc1ad351
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 00:14:30 -0400

    Update non_start.py

--- String-1/non_start.py
+++ String-1/non_start.py
@@ -12,6 +12,7 @@
 def non_start(a, b):
     return a[1:] + b[1:]
 
-print(non_start('Hello', 'There'))
-print(non_start('java', 'code'))
-print(non_start('shotl', 'java'))
+#To check:
+#print(non_start('Hello', 'There'))
+#print(non_start('java', 'code'))
+#print(non_start('shotl', 'java'))
