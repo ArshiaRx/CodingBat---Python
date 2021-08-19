56ae083207ec70af57efca7539f5cd006232cf16
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 02:19:02 -0400

    Update sum3.py

--- List-1/sum3.py
+++ List-1/sum3.py
@@ -16,6 +16,7 @@ def sum3(nums):
         
     return result
 
-print(sum3([1, 2, 3]))
-print(sum3([5, 11 , 2]))
-print(sum3([7, 0, 0]))
+#To check:
+#print(sum3([1, 2, 3]))
+#print(sum3([5, 11 , 2]))
+#print(sum3([7, 0, 0]))
