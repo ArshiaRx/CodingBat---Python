bd5bd8e6eb12702a4a2a2d40e986298041fe32c6
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 00:12:08 -0400

    Update first_two.py

--- String-1/first_two.py
+++ String-1/first_two.py
@@ -17,7 +17,8 @@ def first_two(str):
   else:
     return str[:2]
 
-print(first_two('Hello'))
-print(first_two('abcdefg'))
-print(first_two('ab'))
+#To check
+#print(first_two('Hello'))
+#print(first_two('abcdefg'))
+#print(first_two('ab'))
       
