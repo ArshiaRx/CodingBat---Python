014e6504aa0abb538cbd91f18c584f171c101baa
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 00:11:35 -0400

    Update first_half.py

--- String-1/first_half.py
+++ String-1/first_half.py
@@ -14,6 +14,7 @@ def first_half(str):
   half = len(str) // 2
   return str[:half]
 
-print(first_half('WooHoo'))
-print(first_half('HelloThere'))
-print(first_half('abcdef'))
+#To check
+#print(first_half('WooHoo'))
+#print(first_half('HelloThere'))
+#print(first_half('abcdef'))
