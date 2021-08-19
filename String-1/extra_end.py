389c054aa0554889e3334a5af32256127f3894ad
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 00:11:10 -0400

    Update extra_end.py

--- String-1/extra_end.py
+++ String-1/extra_end.py
@@ -12,7 +12,7 @@
 def extra_end(str):
   return str[len(str)-2:] * 3    #return from 2nd last index up to end * 3
 
-
-print(extra_end('Hello')) 
-print(extra_end('ab'))
-print(extra_end('Hi'))
+#To check
+#print(extra_end('Hello')) 
+#print(extra_end('ab'))
+#print(extra_end('Hi'))
