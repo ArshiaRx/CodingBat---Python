642506ef8f05ac43e3b5d2afa9596bf387800ade
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 02:16:52 -0400

    Update middle_way.py

--- List-1/middle_way.py
+++ List-1/middle_way.py
@@ -12,7 +12,7 @@
 def middle_way(a, b):
   return [a[1], b[1]]
 
-
-print(middle_way([1, 2, 3], [4, 5, 6]))
-print(middle_way([7, 7, 7], [3, 8, 0]))
-print(middle_way([5, 2, 9], [1, 4, 5]))
+#To check:
+#print(middle_way([1, 2, 3], [4, 5, 6]))
+#print(middle_way([7, 7, 7], [3, 8, 0]))
+#print(middle_way([5, 2, 9], [1, 4, 5]))
