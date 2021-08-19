004ce9cc56e95ff31a7c603e924e0838a7b6466d
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 02:14:33 -0400

    Update common_end.py

--- List-1/common_end.py
+++ List-1/common_end.py
@@ -13,7 +13,7 @@
 def common_end(a, b):
     return True if a[0] == b[0] or a[-1] == b[-1] else False
 
-
-print(common_end([1, 2, 3], [7, 3]))
-print(common_end([1, 2, 3], [7, 3, 2]))
-print(common_end([1, 2, 3], [1, 3]))
+#To check:
+#print(common_end([1, 2, 3], [7, 3]))
+#print(common_end([1, 2, 3], [7, 3, 2]))
+#print(common_end([1, 2, 3], [1, 3]))
