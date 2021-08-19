33fbc5821a431a63d6d4a27ac57ef5f20e91fbde
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 02:17:48 -0400

    Update rotate_left3.py

--- List-1/rotate_left3.py
+++ List-1/rotate_left3.py
@@ -12,7 +12,7 @@
 def rotate_left3(nums):
     return [nums[1], nums[2], nums[0]]
 
-
-print(rotate_left3([1, 2, 3]))
-print(rotate_left3([5, 11, 9]))
-print(rotate_left3([7, 0, 0]))
+#To check:
+#print(rotate_left3([1, 2, 3]))
+#print(rotate_left3([5, 11, 9]))
+#print(rotate_left3([7, 0, 0]))
