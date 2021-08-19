191b6dad098d93637b26b84d2800b8b6e72f909c
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-19 02:17:21 -0400

    Update reverse3.py

--- List-1/reverse3.py
+++ List-1/reverse3.py
@@ -13,6 +13,7 @@
 def reverse3(nums):
     return [nums[2], nums[1], nums[0]]
 
-print(reverse3([1, 2, 3]))
-print(reverse3([5, 11, 9]))
-print(reverse3([7, 0, 0]))
+#To check:
+#print(reverse3([1, 2, 3]))
+#print(reverse3([5, 11, 9]))
+#print(reverse3([7, 0, 0]))
