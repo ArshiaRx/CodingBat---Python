e9fb0a1adfb38c53307687fe5e4e51baec612665
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-21 23:24:28 -0400

    Update love6.py

--- Logic-1/love6.py
+++ Logic-1/love6.py
@@ -14,7 +14,7 @@
 def love6(a, b):
   return (a == 6 or b == 6) or (a + b) == 6 or abs(a - b) == 6
 
-
-print(love6(6, 4))
-print(love6(4, 5))
-print(love6(1, 5))
+#To check:
+#print(love6(6, 4))
+#print(love6(4, 5))
+#print(love6(1, 5))
