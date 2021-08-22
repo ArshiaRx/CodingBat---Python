4a80c85e693c58051e50be9fa4aa8c06b9d78e08
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-21 23:25:02 -0400

    Update sorta_sum.py

--- Logic-1/sorta_sum.py
+++ Logic-1/sorta_sum.py
@@ -16,7 +16,7 @@ def sorta_sum(a, b):
   else:
     return sum
 
-print(sorta_sum(3, 4))
-print(sorta_sum(9, 4))
-print(sorta_sum(10, 11))
-    
+#To check:
+#print(sorta_sum(3, 4))
+#print(sorta_sum(9, 4))
+#print(sorta_sum(10, 11))
