975f4124a48de7829856b75350bde090ed5d1c46
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-22 20:09:51 -0400

    Update alarm_clock.py

--- Logic-1/alarm_clock.py
+++ Logic-1/alarm_clock.py
@@ -2,7 +2,6 @@
 
 #alarm_clock
 
-
 #Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean
 #indicating if we are on vacation, return a string of the form "7:00" indicating
 #when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on 
