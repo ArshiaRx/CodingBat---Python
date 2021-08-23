488bc89f08537d9754cce5442a22cce8afe18c99
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-22 20:11:04 -0400

    Update in1to10.py

--- Logic-1/in1to10.py
+++ Logic-1/in1to10.py
@@ -19,6 +19,7 @@ def in1to10(n, outside_mode):
   else:
     return False
 
-print(in1to10(5, False))
-print(in1to10(11, False))
-print(in1to10(11, True))   
+#To check:
+#print(in1to10(5, False))
+#print(in1to10(11, False))
+#print(in1to10(11, True))   
