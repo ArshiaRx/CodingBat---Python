71ab7f1dca7fdcc5f25f69f4401ce6bb36ff4805
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-23 22:21:18 -0400

    Update no_teen_sum.py

--- Logic-2/no_teen_sum.py
+++ Logic-2/no_teen_sum.py
@@ -2,7 +2,6 @@
 
 #no_teen_sum
 
-
 #Given 3 int values, a b c, return their sum. However, if any of the values is 
 #a teen -- in the range 13..19 inclusive -- then that value counts as 0, except
 #15 and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"
