55ac9970e400b38e15d7fd882da933af21591369
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-26 12:05:33 -0400

    Update big_diff.py

--- List-2/big_diff.py
+++ List-2/big_diff.py
@@ -20,8 +20,8 @@ def big_diff(nums):
       small = e
   return big - small
       
-
-print(big_diff([10, 3, 5, 6]))
-print(big_diff([7, 2, 10, 9]))
-print(big_diff([2, 10, 7, 2]))
-
+  
+#To check
+#print(big_diff([10, 3, 5, 6]))
+#print(big_diff([7, 2, 10, 9]))
+#print(big_diff([2, 10, 7, 2]))
