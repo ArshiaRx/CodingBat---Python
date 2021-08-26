ac97027a88409d8f4f0846d71282552b5e58b4b3
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-26 12:06:21 -0400

    Update centered_average.py

--- List-2/centered_average.py
+++ List-2/centered_average.py
@@ -20,6 +20,7 @@ def centered_average(nums):
   return s // n
 
 
-print(centered_average([1, 2, 3, 4, 100]))
-print(centered_average([1, 1, 5, 5, 10, 8, 7]))
-print(centered_average([-10, -4, -2, -4, -2, 0]))
+#To check:
+#print(centered_average([1, 2, 3, 4, 100]))
+#print(centered_average([1, 1, 5, 5, 10, 8, 7]))
+#print(centered_average([-10, -4, -2, -4, -2, 0]))
