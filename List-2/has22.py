dd545bd202d0c39dee7ad21be62ca4977c8d2faf
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-26 12:07:26 -0400

    Update has22.py

--- List-2/has22.py
+++ List-2/has22.py
@@ -25,8 +25,7 @@ def has22(nums):
   return False                            #if otherwise return False
 
 
-print(has22([1, 2, 2]))
-print(has22([1, 2, 1, 2]))
-print(has22([2, 1, 2]))
-
-
+#To check:
+#print(has22([1, 2, 2]))
+#print(has22([1, 2, 1, 2]))
+#print(has22([2, 1, 2]))
