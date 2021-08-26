b70e99f9d8df945388660f8b7907ecb9ebd720ca
ArshiaRx <82693306+ArshiaRx@users.noreply.github.com>
2021-08-26 12:06:56 -0400

    Update count_evens.py

--- List-2/count_evens.py
+++ List-2/count_evens.py
@@ -17,7 +17,7 @@ def count_evens(nums):
             count += 1
     return count
 
-
-print(count_evens([2, 1, 2, 3, 4]))
-print(count_evens([2, 2, 0]))
-print(count_evens([1, 3, 5]))
+#To check:
+#print(count_evens([2, 1, 2, 3, 4]))
+#print(count_evens([2, 2, 0]))
+#print(count_evens([1, 3, 5]))
